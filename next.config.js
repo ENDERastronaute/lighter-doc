6173dc29410610bb4c6dd4fd1ce00825e753fa34
ENDERastronaute <ENDERastronaute@gmail.com>
2024-01-13 15:49:18 +0100

    Fixed basepath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: 'lighter-doc',
+    basePath: '/lighter-doc',
     images: {
         unoptimized: true
     }
